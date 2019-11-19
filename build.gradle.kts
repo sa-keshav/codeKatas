fca25e80a2dcbcbf6bc7d18c5ff1c56a1dde242a
Rolf Rosenbaum <rolf.rosenbaum@rewe-digital.com>
2019-11-19 11:10:06 +0100

    use junit and assertJ

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,6 +15,7 @@ plugins {
 repositories {
     // Use jcenter for resolving your dependencies.
     // You can declare any Maven/Ivy/file repository here.
+    mavenCentral()
     jcenter()
 }
 
@@ -22,11 +23,10 @@ dependencies {
     // Use the Kotlin JDK 8 standard library.
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 
-    // Use the Kotlin test library.
-    testImplementation("org.jetbrains.kotlin:kotlin-test")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.5.1")
 
-    // Use the Kotlin JUnit integration.
-    testImplementation("org.jetbrains.kotlin:kotlin-test-junit")
+    testImplementation("org.assertj:assertj-core")
+
 }
 
 application {
